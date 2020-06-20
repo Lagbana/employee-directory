3e02cdffba87679a3b60aa84600a70f23c0ecf9d
Larry_A <larryagbana@gmail.com>
2020-06-19 22:09:09 -0400

    navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function Navbar () {
+  return (
+    <nav
+      className='navbar navbar-expand-lg'
+      style={{ backgroundColor: '#40E0D0', textAlign: 'center' }}
+    >
+      <Link
+        className='navbar-brand mx-auto'
+        to='/'
+        style={{ color: '#ffffff', display: 'block', textAlign: 'center' }}
+      >
+        <h1>
+          <span className='px-3'>
+            <FontAwesomeIcon icon='layer-group' />
+          </span>
+          Stackify
+        </h1>
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
