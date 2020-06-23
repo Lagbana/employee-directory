a4f74f963a6427e1117c6caeb95559d508d2dd23
Larry_A <larryagbana@gmail.com>
2020-06-23 17:34:01 -0400

    footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,15 @@
+/*
+  Import 
+    *fontawesome icon library components
+    return Footer JSX 
+*/
+
 import React from 'react'
 
 function Footer () {
   return (
     <footer className='footer'>
-      <div className='text-center py-3' style={{backgroundColor: "#E8E8E8", width: "100%"}}>
+      <div className='text-center py-3' style={{backgroundColor: "#E8E8E8", width: "100%", position: "fixed", bottom: 0}}>
         <span>Copyright &copy; 2020 Stackify Inc.</span>
       </div>
     </footer>
