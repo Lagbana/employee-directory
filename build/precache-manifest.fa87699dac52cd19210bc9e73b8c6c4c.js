443c55187a22b2eb3bbf269013d5166ca4ebe488
Larry_A <larryagbana@gmail.com>
2020-06-24 08:56:21 -0400

    heroku deployment

---
+++ build/precache-manifest.fa87699dac52cd19210bc9e73b8c6c4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3296b91e1ddda9ad180d4b93e5cfbee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f66751f215ad3163ec6",
+    "url": "/static/js/2.9a848064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a848064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02906c58cb6d02b10dfe",
+    "url": "/static/js/main.011426a4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
